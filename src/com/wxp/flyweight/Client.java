80bdf2bb9cde5c9287f61344626963a429164cd6
amarsoft <amarsoft@amarsoft-To-be-filled-by-O-E-M>
2020-01-15 10:12:43 +0800

    享元模式

--- src/com/wxp/flyweight/Client.java
+++ src/com/wxp/flyweight/Client.java
@@ -4,10 +4,15 @@ public class Client {
 public static void main(String[] args) {
 	WebsiteFactory websiteFactory = new WebsiteFactory();
 	ConcreteWebsite concreteWebsite1 =(ConcreteWebsite) websiteFactory.getConcWebsite("博客");
+	concreteWebsite1.use();
 	ConcreteWebsite concreteWebsite2 =(ConcreteWebsite) websiteFactory.getConcWebsite("网页");
+	concreteWebsite2.use();
 	ConcreteWebsite concreteWebsite3 =(ConcreteWebsite) websiteFactory.getConcWebsite("网页");
+	concreteWebsite3.use();
 	ConcreteWebsite concreteWebsite4 =(ConcreteWebsite) websiteFactory.getConcWebsite("网页");
+	concreteWebsite4.use();
 	ConcreteWebsite concreteWebsite5 =(ConcreteWebsite) websiteFactory.getConcWebsite("网页");
+	concreteWebsite5.use();
 	System.out.println(websiteFactory.getCacheMap().size());//只有两个，所以初始化了两个对象。
 }
 }
